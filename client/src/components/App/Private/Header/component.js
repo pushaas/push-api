71df9d3f7780cb55f48d6bbf518648f43f6f5235
rafaeleyng <rafaeleyng@gmail.com>
2019-06-29 01:28:54 -0300

    comment notification icon

--- client/src/components/App/Private/Header/component.js
+++ client/src/components/App/Private/Header/component.js
@@ -4,9 +4,9 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import Badge from '@material-ui/core/Badge'
+// import Badge from '@material-ui/core/Badge'
 import MenuIcon from '@material-ui/icons/Menu'
-import NotificationsIcon from '@material-ui/icons/Notifications'
+// import NotificationsIcon from '@material-ui/icons/Notifications'
 
 import { makeUseStylesHook } from 'components/App/styles'
 
@@ -27,11 +27,11 @@ const Header = ({ open, handleDrawerOpen }) => {
         <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
           Stats
         </Typography>
-        <IconButton color="inherit">
+        {/* <IconButton color="inherit">
           <Badge badgeContent={4} color="secondary">
             <NotificationsIcon />
           </Badge>
-        </IconButton>
+        </IconButton> */}
       </Toolbar>
     </AppBar>
   )
