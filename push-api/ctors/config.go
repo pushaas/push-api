72a4db95727736b01e9a80ebdba1e834eba99356
rafaeleyng <rafaeleyng@gmail.com>
2019-07-05 23:10:07 -0300

    fix docker for prod

--- push-api/ctors/config.go
+++ push-api/ctors/config.go
@@ -63,6 +63,7 @@ func setupFromDefaults(config *viper.Viper, env string) {
 	// api
 	config.SetDefault("api.enable_auth", true)
 	config.SetDefault("api.basic_auth_user", "app")
+	config.SetDefault("api.basic_auth_password", "abc123")
 	config.SetDefault("api.statics_path", "./client/build")
 
 	// push_stream
