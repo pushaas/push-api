37243c7ee112f9bf778f001997380d243daed0b8
Diego Chavão <diego.chavao@corp.globo.com>
2020-07-06 09:23:39 -0300

    Add support for redis sentinel

--- push-api/ctors/redis.go
+++ push-api/ctors/redis.go
@@ -2,6 +2,8 @@ package ctors
 
 import (
 	"errors"
+	"net/url"
+	"strings"
 
 	"github.com/go-redis/redis"
 	"github.com/spf13/viper"
@@ -12,19 +14,60 @@ func getRedisUrl(config *viper.Viper) string {
 	return config.GetString("redis.url")
 }
 
-func getRedisOptions(config *viper.Viper) (*redis.Options, error) {
-	url := getRedisUrl(config)
-	options, err := redis.ParseURL(url)
+func getSentinelMasterName(config *viper.Viper) string {
+	return config.GetString("redis.sentinel.master_name")
+}
+
+func convertToUniversalOptions(options *redis.Options) *redis.UniversalOptions {
+	return &redis.UniversalOptions{
+		Addrs:              []string{options.Addr},
+		Password:           options.Password,
+		DB:                 options.DB,
+		MaxRetries:         options.MaxRetries,
+		OnConnect:          options.OnConnect,
+		MinRetryBackoff:    options.MinRetryBackoff,
+		MaxRetryBackoff:    options.MaxRetryBackoff,
+		DialTimeout:        options.DialTimeout,
+		ReadTimeout:        options.ReadTimeout,
+		WriteTimeout:       options.WriteTimeout,
+		PoolSize:           options.PoolSize,
+		MinIdleConns:       options.MinIdleConns,
+		MaxConnAge:         options.MaxConnAge,
+		PoolTimeout:        options.PoolTimeout,
+		IdleTimeout:        options.IdleTimeout,
+		IdleCheckFrequency: options.IdleCheckFrequency,
+		TLSConfig:          options.TLSConfig,
+	}
+}
+
+func getRedisOptions(config *viper.Viper) (*redis.UniversalOptions, error) {
+	redisUrl := getRedisUrl(config)
+	options, err := redis.ParseURL(redisUrl)
 
 	if err != nil {
-		return nil, errors.New("failed to parse redis URL")
+		urlOptions, err := url.Parse(redisUrl)
+
+		if err != nil {
+			return nil, errors.New("failed to parse redis URL")
+		}
+
+		password, _ := urlOptions.User.Password()
+		addresses := strings.Split(urlOptions.Host, ",")
+
+		return &redis.UniversalOptions{
+			MasterName: getSentinelMasterName(config),
+			Addrs:      addresses,
+			Password:   password,
+		}, nil
 	}
 
 	if options.Addr == "" {
 		return nil, errors.New("redis URL is required")
 	}
 
-	return options, nil
+	universalOptions := convertToUniversalOptions(options)
+
+	return universalOptions, nil
 }
 
 func NewRedisClient(config *viper.Viper, logger *zap.Logger) (redis.UniversalClient, error) {
@@ -36,7 +79,8 @@ func NewRedisClient(config *viper.Viper, logger *zap.Logger) (redis.UniversalCli
 		return nil, err
 	}
 
-	log.Info("initializing redis with address", zap.String("addr", options.Addr))
-	client := redis.NewClient(options)
+	log.Info("initializing redis with address", zap.String("addr", options.Addrs[0]))
+	client := redis.NewUniversalClient(options)
+
 	return client, nil
 }
